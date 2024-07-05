58e4c970d2835835b3e824e673dc8d167466da18
hieunmhe171547 <hieubeo5d@gmail.com>
2024-07-05 12:43:47 +0700

    1.Get current location 2.Zoom and rotate map using button or gesture

--- app/src/main/java/com/example/googlemapactivity/MainActivity.java
+++ app/src/main/java/com/example/googlemapactivity/MainActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
