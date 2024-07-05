d091c9437560c07d5dcc5ca07c7205a545384c3d
hieunmhe171547 <hieubeo5d@gmail.com>
2024-07-05 12:43:11 +0700

    1.Get current location 2.Zoom and rotate map using button or gesture

--- app/src/main/java/com/example/googlemapactivity/MainActivity.java
+++ app/src/main/java/com/example/googlemapactivity/MainActivity.java
@@ -1,47 +1,91 @@
 package com.example.googlemapactivity;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.id_map);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+        ViewMapByType();
+//        Đừng xóa cmt 2 dòng dưới để test
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+//                    mapFragment.getMapAsync(MainActivity.this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng myLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());// Hanoi(21,105):
+//        LatLng myLocation = new LatLng(21,105); // Tọa độ Hà Nội :D
+        MarkerOptions options = new MarkerOptions().position(myLocation).title("My Location");
+        myMap.addMarker(options);
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+        myMap.getUiSettings().setZoomControlsEnabled(true);
+        myMap.getUiSettings().setCompassEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Please allow permission to get current location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void ViewMapByType() {
         Spinner mapTypeSpinner = findViewById(R.id.mapTypeSpinner);
         mapTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (mMap != null) {
+                if (myMap != null) {
                     switch (position) {
                         case 0:
-                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                            myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                             break;
                         case 1:
-                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                            myMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                             break;
                         case 2:
-                            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                            myMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
                             break;
                     }
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // Do nothing
@@ -49,13 +93,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+                    mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
     }
+
 }
